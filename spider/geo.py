a261dc3ce9e43c00e2ae09031e24dfbeb7b2497c
Shankai Yan <sk.yan@my.cityu.edu.hk>
2017-07-02 05:05:40 +0000

    Add new GEO client that based on GEOparse, and rename the original one to geoxml.py

--- spider/geo.py
+++ spider/geo.py
@@ -5,22 +5,14 @@
 # File Name: geo.py
 # Author: Shankai Yan
 # E-mail: sk.yan@my.cityu.edu.hk
-# Created Time: 2016-10-14 21:04:40
+# Created Time: 2017-05-25 18:28:46
 ###########################################################################
 #
 
 import os
-import re
 import sys
-import urllib
-import urllib2
-import codecs
-import xmlextrc
 
-from cStringIO import StringIO
-from abc import ABCMeta
-
-import pandas as pd
+import GEOparse
 
 from .. import nlp
 from ..util import fs
@@ -31,238 +23,83 @@ if sys.platform.startswith('win32'):
 elif sys.platform.startswith('linux2'):
 	DATA_PATH = os.path.join(os.path.expanduser('~'), 'data', 'bionlp')
 GEO_PATH = os.path.join(DATA_PATH, 'geo')
+SC=';;'
 
 
-class GSEBuilder():
-#	__metaclass__ = ABCMeta
-
-	def __init__(self, view='brief'):
-		self.view = view
-		self._tag = ''
-		self._tag_stack = []
-		self.db = ''
-		self.submit_date = ''
-		self.release_date = ''
-		self.last_update = ''
-		self.samples = []
-		
-		self.ftype = 'SERIES'	# GEO type [SAMPLE | SERIES | PLATFORM | DATASET]
-		self.id = ''	# GEO Accession
-		self.pmid = ''
-		self.title = ''
-		self.summary = ''
-		
-	def start(self, tag, attrib):
-		self._tag = tag.split('}')[1] if '}' in tag else tag
-		self._tag_stack.append(self._tag)
-		# Process the attributes
-		if (self._tag == 'Database'):
-			self.db = attrib['iid']
-		if (self._tag == 'Series'):
-			self.id = attrib['iid']
-		if (self._tag == 'Sample'):
-			self.samples.append(attrib['iid'])
-	
-	def end(self, tag):
-		self._tag_stack.pop()
-		pass
-		
-	def data(self, data):
-		if data.isspace():
-			return
-		data = data.strip()
-		# Process the text content
-		if (self._tag == 'Submission-Date'):
-			self.submit_date = data
-		if (self._tag == 'Release-Date'):
-			self.release_date = data
-		if (self._tag == 'Last-Update-Date'):
-			self.last_update = data
-
-		if (self._tag == 'Pubmed-ID'):
-			self.pmid = data
-		if (self._tag == 'Title'):
-			self.title = data
-		if (self._tag == 'Summary'):
-			self.keywords = []
-			paragraphs = data.split('\n')
-			i = len(paragraphs) - 1
-			for i in xrange(len(paragraphs) - 1, 0, -1):
-				if (paragraphs[i].startswith('Keywords')):
-					phrases = paragraphs[i].split(':')
-					if (len(phrases) == 1):
-						phrases = paragraphs[i].split('=')
-					self.keywords.append(phrases[1].strip())
-				else:
-					break
-			self.keywords.reverse()
-			self.summary = '\n'.join(paragraphs[:i+1])
-		
-	def close(self):
-		pass
-
-	def build(self):
-		return {'db':self.db, 'submit_date':self.submit_date, 'release_date':self.release_date, 'last_update':self.last_update, 'samples':self.samples, 'id':self.id, 'ftype':self.ftype, 'pmid':self.pmid, 'title':self.title, 'summary':self.summary, 'keywords':';'.join(self.keywords)}
-		
-		
-class GSMBuilder():
-#	__metaclass__ = ABCMeta
-
-	def __init__(self, view='brief'):
-		self.view = view
-		self._tag = ''
-		self._tag_stack = []
-		self.platform = ''
-		self.db = ''
-		self.submit_date = ''
-		self.release_date = ''
-		self.last_update = ''
-		
-		self.ftype = 'SAMPLE'	# GEO type [SAMPLE | SERIES | PLATFORM | DATASET]
-		self.id = ''	# GEO Accession
-		self.title = ''
-		self.description = ''
-		self.type = ''
-		self.source = ''
-		self.organism = ''
-		self.tissue = ''
-		self.tissue_type = ''
-		self.trait = []
-		
-		# Internal data
-		self.enter_dt = False
-		self.enter_col = False
-		self.col_pos = []
-		self.col_name = []
-		self.row_num = 0
-		self.df = None
-		
-	def start(self, tag, attrib):
-		self._tag = tag.split('}')[1] if '}' in tag else tag # Remove the namespace
-		self._tag_stack.append(self._tag)
-		# Process the attributes
-		if (self._tag == 'Platform'):
-			self.platform = attrib['iid']
-		if (self._tag == 'Database'):
-			self.db = attrib['iid']
-		if (self._tag == 'Sample'):
-			self.id = attrib['iid']
-		if (self._tag == 'Characteristics'):
-			self._subtag = attrib['tag'] if attrib.has_key('tag') else ''
-
-		if (self.view != 'full'): return
-		if (self._tag == 'Data-Table'):
-			self.enter_dt = True
-		if (self._tag == 'Column'):
-			self.enter_col = True
-			if (self.enter_dt):
-				self.col_pos.append(int(attrib['position']))
-
-		if (self._tag == 'Internal-Data' and self.enter_dt):
-			self.row_num = int(attrib['rows'])
-	
-	def end(self, tag):
-		self._tag = tag.split('}')[1] if '}' in tag else tag # Remove the namespace
-		self._tag_stack.pop()
-		if (self.view != 'full'): return
-		if (self._tag == 'Data-Table'):
-			self.enter_dt = False
-		if (self._tag == 'Column'):
-			self.enter_col = False
-		
-	def data(self, data):
-		if data.isspace(): return
-		data = data.strip()
-		# Process the text content
-		if (self._tag == 'Submission-Date'):
-			self.submit_date = data
-		if (self._tag == 'Release-Date'):
-			self.release_date = data
-		if (self._tag == 'Last-Update-Date'):
-			self.last_update = data
-
-		if (self._tag == 'Title'):
-			self.title = data
-		if (self._tag == 'Description'):
-			if (self._tag_stack[-2] == 'Sample'):
-				self.description = data
-		if (self._tag == 'Type'):
-			self.type = data
-		if (self._tag == 'Source'):
-			self.source = data	
-		if (self._tag == 'Organism'):
-			self.organism = data
-		if (self._tag == 'Characteristics'):
-			if (self._subtag == 'tissue'):
-				self.tissue = data
-			elif (self._subtag == 'tissue type'):
-				self.tissue_type = data
-			self.trait.append(data)
-			
-		if (self.view != 'full'): return
-		if (self._tag == 'Name' and self.enter_dt and self.enter_col):
-			self.col_name.append(data)
-		if (self._tag == 'Internal-Data'):
-			data_sio = StringIO('\n'.join(['\t'.join(self.col_name), data.replace('\"', '')]))
-			if (self.df is None):
-				self.df = pd.read_csv(data_sio, sep='\t', header=0, index_col=0, na_values=['null']).fillna(value=0)
-			else:
-				self.df = self.df.append(pd.read_csv(data_sio, sep='\t', header=0, index_col=0, na_values=['null']).fillna(value=0))
-			
-		
-	def close(self):
-		pass
-
-	def build(self):
-		return {'platform':self.platform,'db':self.db, 'submit_date':self.submit_date, 'release_date':self.release_date, 'last_update':self.last_update, 'id':self.id, 'ftype':self.ftype, 'title':self.title, 'description':self.description, 'type':self.type, 'source':self.source, 'organism':self.organism, 'tissue':self.tissue, 'tissue_type':self.tissue_type, 'trait':','.join(self.trait), 'data':self.df}
-		
-		
-BUILDER_MAP = {'gse':GSEBuilder, 'gsm':GSMBuilder}
+GSE_KEYMAP = {'platform_id':'platform', 'submission_date':'submit_date', 'last_update_date':'last_update', 'sample_id':'samples', 'geo_accession':'id', 'pubmed_id':'pmid', 'title':'title', 'summary':'summary'}
+GSM_KEYMAP = {'platform_id':'platform', 'submission_date':'submit_date', 'last_update_date':'last_update', 'geo_accession':'id', 'title':'title', 'description':'description', 'data_processing':'data_processing', 'source_name_ch1':'source', 'organism_ch1':'organism', '':'tissue', '':'tissue_type', 'treatment_protocol_ch1':'treat_protocol', 'growth_protocol_ch1':'growth_protocol', 'extract_protocol_ch1':'extract_protocol', 'label_protocol_ch1':'label_protocol', 'label_ch1':'label', 'hyb_protocol':'hybrid_protocol', 'scan_protocol':'scan_protocol', 'characteristics_ch1':'trait', 'table':'data'}
 
 
-def fetch_geo(accessions, type='self', view='full', fmt='xml', saved_path=GEO_PATH):
-	fmt2ext = {'text':'txt', 'xml':'xml', 'html':'html'}
+def fetch_geo(accessions, saved_path=GEO_PATH, skip_cached=False):
 	fs.mkdir(saved_path)
+	geo_set = set(accessions)
+	count = 0
+	for acc in geo_set:
+		cachef = os.path.join(saved_path, '%s_family.soft.gz' % acc)
+		if (os.path.exists(cachef)):
+			if (skip_cached): continue
+			res = GEOparse.get_GEO(filepath=cachef)
+		else:
+			try:
+				res = GEOparse.get_GEO(geo=acc, destdir=saved_path)
+			except:
+				print 'Failed to fetch the GEO file: %s' % acc
+				continue
+		count += 1
+		yield res
+	print "Number of obtained GEO documents: %i\n" % len(geo_set)
+
+
+def parse_geo(gse, view='brief', with_samp=True):
+	samples = []
+	if (with_samp):
+		for gsm_id, gsm in gse.gsms.items():
+			samples.append((gsm_id, update_keys(gsm.metadata, type='GSM')))
+	return (gse.get_accession(), update_keys(gse.metadata, type='GSE'), samples)
+	
+def parse_geos(gses, view='brief', with_samp=True):
 	results = []
-	for acc in set(accessions):
-		tgt_file = os.path.join(saved_path, acc + '.%s' % fmt2ext[fmt])
-		if os.path.exists(tgt_file): continue
-		url = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=%s&targ=%s&view=%s&form=%s" % (acc, type, view, fmt)
-		try:
-			res = urllib2.urlopen(url).read()
-		except:
-			print 'Failed to fetch the GEO file: %s' % acc
-		fs.write_file(res, tgt_file)
-		results.append(res)
-	print "Number of obtained GEO documents: %i\n" % len(results)
+	for gse in gses:
+		results.append(parse_geo(gse, view=view, with_samp=with_samp))
 	return results
 	
-	
-def parse_geo(geo_fpath, view='brief', type='gse', fmt='xml'):
-	Builder = BUILDER_MAP[type]
-	builder = Builder(view=view)
-	geo_str = fs.read_file(geo_fpath)
-	parser = xmlextrc.get_parser(builder)
-	try:
-		parser.feed(nlp.clean_text('\n'.join(geo_str), encoding='utf-8', replacement=None))
-	except Exception as err:
-		print 'Can not parse the file: %s' % geo_fpath
-		raise err
-	parser.close()
-	return builder.build()
-	
-	
-def parse_geos(geo_fpaths, view='brief', type='gse', fmt='xml'):
-	Builder = BUILDER_MAP[type]
-	results = []
-	for i, geo_str in enumerate(fs.read_files(geo_fpaths)):
-		builder = Builder(view=view)
-		parser = xmlextrc.get_parser(builder)
-		try:
-			parser.feed(nlp.clean_text(re.sub(u'[\x00-\x08\x0b-\x0c\x0e-\x1f]+', u'', '\n'.join(geo_str)), encoding='utf-8', replacement=None))
-		except Exception as err:
-			print 'Can not parse the file: %s' % geo_fpaths[i]
-			raise err
-		parser.close()
-		results.append(builder.build())
-	return results
+def update_keys(data, type='GSE'):
+	data['ftype'] = 'SERIES'if type.upper() == 'GSE' else 'SAMPLE'
+	key_map = GSE_KEYMAP if type.upper() == 'GSE' else GSM_KEYMAP
+	for k, v in key_map.iteritems():
+		if (data.has_key(k)):
+			tmp = data[k]
+			del data[k]
+			data[v] = tmp
+		else:
+			data[v] = ''
+			continue
+		# Unroll some common attributes
+		for key in ['platform', 'submit_date', 'last_update', 'id', 'title']:
+			if (v == key):
+				data[key] = data[key][0]
+		if (type.upper() == 'GSE'):
+			if (v == 'pmid'):
+				data['pmid'] = data['pmid'][0]
+			if (v == 'summary'):
+				keywords = []
+				paragraphs = data['summary']
+				for i in xrange(len(paragraphs) - 1, -1, -1):
+					if (paragraphs[i].startswith('Keywords')):
+						phrases = paragraphs[i].split(':')
+						if (len(phrases) == 1):
+							phrases = paragraphs[i].split('=')
+						keywords.append(phrases[1].strip())
+					else:
+						data['summary'] = '\n'.join(paragraphs[:i+1])
+						break
+				keywords.reverse()
+				data['keywords'] = SC.join(keywords)
+		elif (type.upper() == 'GSM'):
+			# Unroll some GSM attributes
+			for key in ['description', 'type', 'source', 'organism', 'treat_protocol', 'growth_protocol', 'extract_protocol', 'label_protocol', 'label', 'hybrid_protocol', 'scan_protocol']:
+				if (v == key):
+					data[key] = data[key][0]
+			if (v == 'trait'):
+				data[v] = SC.join(data[v])
+	return data
