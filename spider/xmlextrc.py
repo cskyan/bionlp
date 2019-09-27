9560fe57b69359d74a61458711a71cb95594fa72
cskyan <dr.skyan@gmail.com>
2019-09-27 17:07:21 -0400

    Fix bugs

--- spider/xmlextrc.py
+++ spider/xmlextrc.py
@@ -20,10 +20,10 @@ def get_parser(builder, encoding='utf-8', **kwargs):
 def extrc_list(listhead_path, elem_tag, content_path, xml_tree=None, xml_str='', ret_type='text'):
 	if (xml_tree is None):
 		try:
-			xml_tree = ET.fromstring(xml_str.encode('utf-8'))
+			xml_tree = ET.fromstring(xml_str.decode('utf-8'))
 		except ParseError as e:
 			print(e)
-			print(xml_str.encode('utf-8').strip())
+			print(xml_str.decode('utf-8').strip())
 	if (xml_tree.tag == listhead_path):
 		list_root = xml_tree
 	else:
