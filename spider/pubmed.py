8a33f4c4f050b85572b72a0a4d3497afc3dc5b2b
cskyan <dr.skyan@gmail.com>
2019-10-11 19:37:34 -0400

    Fix bugs, add functions to retrieve the PubMed XML archive in parallel

--- spider/pubmed.py
+++ spider/pubmed.py
@@ -9,11 +9,11 @@
 ###########################################################################
 #
 
-import os, sys, time, codecs, urllib
+import os, sys, time, codecs, urllib, itertools
 
 # from ..util import fs
 # from . import xmlextrc
-from bionlp.util import fs
+from bionlp.util import fs, njobs
 from bionlp.spider import xmlextrc
 
 if sys.platform.startswith('win32'):
@@ -176,6 +176,39 @@ def fetch_abs(pmid_list, saved_path=ABS_PATH):
 	return abs_text
 
 
+def _save_feat(articles, feat_attr, saved_path=DATA_PATH):
+	for artcl in articles:
+		for f, a in feat_attr.items():
+			if (hasattr(artcl[f], '__iter__')):
+				attr_text = '\n'.join([':'.join(x) for x in artcl[f]] if artcl[f] and type(artcl[f][0]) is tuple else artcl[f])
+			else:
+				attr_text = artcl[f]
+			fs.write_file(attr_text, os.path.join(saved_path, a[0], artcl['pmid']+'.'+a[1]), code='utf8')
+		print('Processed PMID:%s' % artcl['pmid'])
+		sys.stdout.flush()
+
+
+def process_artcls(xml_str, saved_path=DATA_PATH, n_jobs=1):
+	artcl_xml = xmlextrc.extrc_list('PubmedArticleSet', 'PubmedArticle', '.', None, xml_str, 'xml')
+	articles = []
+	for artcl in artcl_xml:
+		builder = PubMedBuilder()
+		parser = xmlextrc.get_parser(builder)
+		parser.feed(artcl)
+		articles.append(builder.build())
+	print("Number of obtained articles: %i\n" % len(artcl_xml))
+	if saved_path is not None:
+		feat_attr = {'title':['title', 'txt'], 'abs':['abs', 'txt'], 'kws':['kws', 'txt'], 'mesh':['mesh', 'mesh'], 'chem':['chem', 'chem']}
+		for f, a in feat_attr.items():
+			fs.mkdir(os.path.join(saved_path, a[0]))
+		if n_jobs == 1:
+			_save_feat(articles, feat_attr, saved_path=saved_path)
+		else:
+			task_bnd = njobs.split_1d(len(articles), split_num=n_jobs, ret_idx=True)
+			_ = njobs.run_pool(_save_feat, n_jobs=n_jobs, dist_param=['articles'], articles=[articles[task_bnd[i]:task_bnd[i+1]] for i in range(n_jobs)], feat_attr=feat_attr, saved_path=saved_path)
+	return articles
+
+
 def fetch_artcls(pmid_list, saved_path=DATA_PATH, max_trail=-1):
 	url = BASE_URL + "efetch.fcgi?db=pubmed&retmode=xml&id=" + ','.join(pmid_list)
 	print("Fetch String:\n" + url + '\n')
@@ -188,27 +221,33 @@ def fetch_artcls(pmid_list, saved_path=DATA_PATH, max_trail=-1):
 			print(e)
 			time.sleep(1)
 			trail += 1
+	return process_artcls(res, saved_path=saved_path)
 
-	artcl_xml = xmlextrc.extrc_list('PubmedArticleSet', 'PubmedArticle', '.', None, res, 'xml')
-	articles = []
-	for artcl in artcl_xml:
-		builder = PubMedBuilder()
-		parser = xmlextrc.get_parser(builder)
-		parser.feed(artcl)
-		articles.append(builder.build())
-	print("Number of obtained articles: %i\n" % len(artcl_xml))
-	if saved_path is not None:
-		feat_attr = {'abs':['abs', 'txt'], 'kws':['kws', 'txt'], 'mesh':['mesh', 'mesh'], 'chem':['chem', 'chem']}
-		for f, a in feat_attr.items():
-			fs.mkdir(os.path.join(saved_path, a[0]))
-		for id, artcls in zip(pmid_list, articles):
-			for f, a in feat_attr.items():
-				if (hasattr(artcls[f], '__iter__')):
-					attr_text = '\n'.join([':'.join(x) for x in artcls[f]] if artcls[f] and type(artcls[f][0]) is tuple else artcls[f])
-				else:
-					attr_text = artcls[f]
-				fs.write_file(attr_text, os.path.join(saved_path, a[0], str(id)+'.'+a[1]), code='utf8')
+
+def _fetch_archive(archive_files, saved_path=DATA_PATH, write_n_jobs=1):
+	import builtins, gzip
+	articles, open, fmode, open_kwargs = [], getattr(builtins, 'open'), 'r', {'encoding':'utf-8'}
+	for fpath in archive_files:
+		sys.stdout.flush()
+		if fpath.endswith('.gz'): open, fmode, open_kwargs = gzip.open, 'rb', {}
+		with open(fpath, fmode, **open_kwargs) as fd:
+			res = fd.read()
+		articles.extend(process_artcls(res, saved_path=saved_path, n_jobs=write_n_jobs))
+		print("Processed archive file %s" % os.path.basename(fpath))
+		sys.stdout.flush()
 	return articles
+
+
+def fetch_archive(archive_paths, saved_path=DATA_PATH, n_jobs=(1,1)):
+	archive_paths, articles = archive_paths if type(archive_paths) is list else archive_paths, []
+	all_archive_files = list(itertools.chain.from_iterable([fs.listf(archive_path, full_path=True) for archive_path in archive_paths]))
+	n_jobs, write_n_jobs = n_jobs if hasattr(n_jobs, '__iter__') else (n_jobs, 1)
+	if n_jobs == 1:
+		return _fetch_archive(all_archive_files, saved_path=saved_path)
+	else:
+		task_bnd = njobs.split_1d(len(all_archive_files), split_num=n_jobs, ret_idx=True)
+		articles_grps = njobs.run_pool(_fetch_archive, n_jobs=n_jobs, dist_param=['archive_files'], archive_files=[all_archive_files[task_bnd[i]:task_bnd[i+1]] for i in range(n_jobs)])
+		return list(itertools.chain.from_iterable(articles_grps))
 
 
 if __name__ == '__main__':
