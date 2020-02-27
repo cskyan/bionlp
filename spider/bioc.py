99514989e6fb2a2fd8d4da6fb2f05dcdc0f892dd
cskyan <dr.skyan@gmail.com>
2020-02-27 16:52:55 -0500

    Add a wrapper for BioC API

---
+++ spider/bioc.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+###########################################################################
+# Copyright (C) 2013-2019 by Caspar. All rights reserved.
+# File Name: bioc.py
+# Author: Shankai Yan
+# E-mail: shankai.yan@nih.gov
+# Created Time: 2020-02-25 17:17:35
+###########################################################################
+#
+
+import os, re, sys, copy, time, json, string
+from contextlib import ExitStack
+
+from nltk.tokenize import sent_tokenize, TreebankWordTokenizer
+
+from apiclient import APIClient
+
+import spacy
+import ftfy, spacy
+try:
+    nlp = spacy.load('en_core_sci_md')
+except Exception as e:
+    print(e)
+    try:
+        nlp = spacy.load('en_core_sci_sm')
+    except Exception as e:
+        print(e)
+        nlp = spacy.load('en_core_web_sm')
+
+
+if sys.platform.startswith('win32'):
+	DATA_PATH = 'D:\\data\\bionlp'
+elif sys.platform.startswith('linux'):
+	DATA_PATH = os.path.join(os.path.expanduser('~'), 'data', 'bionlp')
+SC=';;'
+
+
+def preprocess(text, tokenize=False):
+    text = text.lstrip('nullnullnull').strip()
+    if len(text) > 2:
+        text = re.sub(r'[\t\r\n]+', ' ', text)
+        text = re.sub(r'[^\x00-\x7F]+', ' ', text)
+        if tokenize:
+            tokenized = TreebankWordTokenizer().tokenize(text)
+            text = ' '.join(tokenized)
+        # to address single quotation mark issue
+        text = re.sub(r"^'(\w)", r"' \1", text)
+        text = re.sub(r"\s'(\w)", r" ' \1", text)
+        # to address 's issue
+        text = re.sub(r"\s's\b", "'s", text)
+        return text
+    else:
+        return None
+
+
+def fetch_artcls(all_pmids, out_fpaths=None, pid=0, cache=False, abs_only=False, retdoc=False):
+	client = BioCAPI(function='pubmed') if abs_only else BioCAPI(function='pmc')
+	# res = [client.call(id=pmid, encoding='ascii') for pmid in all_pmids]
+	# res = [dict(id=doc['documents'][0]['id'], text=' \n '.join([span['text'] for span in doc['documents'][0]['passages']])) for doc in res]
+	res = []
+	with ExitStack() as stack:
+		if out_fpaths is not None:
+			try:
+				outfile_pmid, outfile0, outfile1, outfile2, outfile3 = out_fpaths
+				fout = stack.enter_context(open(outfile_pmid, 'a+', encoding='utf-8'))
+				# fout_list = [stack.enter_context(open(f, 'a', encoding='utf-8')) if f else None for f in [outfile0, outfile1, outfile2, outfile3]]
+				fout_list = [stack.enter_context(open(f, 'a', encoding='utf-8')) if f else None for f in [None, None, outfile2, outfile3]]
+				pmids = set([int(pmid.rstrip('\n')) for pmid in fout.readlines()]) if cache else set([])
+			except Exception as e:
+				print(e)
+				out_fpaths = None
+		for i, pmid in enumerate(all_pmids):
+			if out_fpaths and pmid in pmids: continue
+			doc = client.call(id=pmid, encoding='ascii')
+			if len(doc) == 0: continue
+			id, text = doc['documents'][0]['id'], preprocess(' '.join([span['text'] if span['text'] and span['text'][-1] in string.punctuation else '%s.' % span['text'] for span in doc['documents'][0]['passages']]))
+			if not text or text.isspace(): continue
+			fout_list[:2] = [stack.enter_context(open((lambda x: '%s_%s%s'%(x[0], pmid, x[1]))(os.path.splitext(f)), 'a', encoding='utf-8')) if f else None for f in [outfile0, outfile1]]
+			if out_fpaths:
+				if outfile0: fout_list[0].write(text + '\n')
+				if outfile1: fout_list[1].write(text.lower() + '\n')
+				if outfile2 or outfile3:
+					for sent in nlp(text).sents:
+						if not sent.text or sent.text.isspace(): continue
+						if outfile2: fout_list[2].write(sent.text + '\n')
+						if outfile3: fout_list[3].write(sent.text.lower() + '\n')
+				fout.write(str(pmid) + '\n')
+			if i and i % 10000 == 0:
+			   print('Retrieved %i documents ' % i)
+			   sys.stdout.flush()
+			if retdoc: res.append(dict(id=id, text=text))
+	return res if retdoc else None
+
+
+class BioCAPI(APIClient, object):
+	BASE_URL = 'https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful'
+	_function_url = {'pubmed':'pubmed.cgi', 'pmc':'pmcoa.cgi'}
+	_default_param = {'pubmed':dict(id='', encoding='ascii'), 'pmc':dict(id='', encoding='ascii')}
+	_func_restype = {'pubmed':'json', 'pmc':'json'}
+
+	def __init__(self, function='annotate'):
+		if (function not in self._default_param):
+			raise ValueError('The function %s is not supported!' % function)
+		APIClient.__init__(self)
+		self.function = function
+		self.func_url = self._function_url[function]
+		self.restype = self._func_restype.setdefault(function, 'json')
+
+	def _handle_response(self, response):
+		if (self.restype == 'xml'):
+			pass
+		elif (self.restype == 'json'):
+			res = response.data
+			try:
+				res_json = json.loads(res)
+			except Exception as e:
+				print(e)
+				print(res)
+				res_json = {}
+			return res_json
+
+	def call(self, max_trail=-1, interval=3, **kwargs):
+		args = copy.deepcopy(self._default_param[self.function])
+		args.update((k, v) for k, v in kwargs.items() if k in args)
+		trail = 0
+		while max_trail <= 0 or trail < max_trail:
+			try:
+				res = APIClient.call(self, '/%s/BioC_%s/%s/%s' % (self.func_url, self.restype, kwargs['id'], kwargs['encoding']))
+				break
+			except Exception as e:
+				print(e)
+				time.sleep(interval)
+				trail += 1
+		return res
+
+if __name__ == '__main__':
+	print([doc for doc in fetch_artcls(['29378527'], out_fpaths=['pmid.txt', 'doc.txt', None, None, None], retdoc=True)])
