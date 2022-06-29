6722ac0738eb5f6449b329077aebc47b4c2b6bd7
cskyan <dr.skyan@gmail.com>
2022-06-29 23:26:34 +0800

    Add class InteractiveCMD

--- util/shell.py
+++ util/shell.py
@@ -10,7 +10,7 @@
 #
 
 import time
-import subprocess, shlex
+import subprocess, queue, threading, shlex
 
 
 def daemon(cmd, proc_name, interval=3):
@@ -20,7 +20,7 @@ def daemon(cmd, proc_name, interval=3):
 			subprocess.call(cmd, shell=True)
 		time.sleep(interval)
 
-	
+
 def check_numproc(proc_name):
 	ps = subprocess.Popen(shlex.split('ps -ef'), stdout=subprocess.PIPE)
 	grep = subprocess.Popen(shlex.split('grep %s'%proc_name), stdin=ps.stdout, stdout=subprocess.PIPE)
@@ -28,3 +28,52 @@ def check_numproc(proc_name):
 	wc = subprocess.Popen(shlex.split('wc -l'), stdin=grepv.stdout, stdout=subprocess.PIPE)
 	numproc = int(wc.communicate()[0])
 	return numproc
+
+
+class InteractiveCMD(object):
+    def __init__(self, cmd):
+    	self.cmd = cmd
+    	self.p = subprocess.Popen(self.cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, universal_newlines=True)
+    	self.out_queue = queue.Queue()
+    	self.err_queue = queue.Queue()
+
+    def start(self, verbose=False):
+        out_thread = threading.Thread(target=InteractiveCMD._enqueue_output, args=(self.p.stdout, self.out_queue))
+        err_thread = threading.Thread(target=InteractiveCMD._enqueue_output, args=(self.p.stderr, self.err_queue))
+        out_thread.daemon = True
+        err_thread.daemon = True
+        out_thread.start()
+        err_thread.start()
+        if verbose:
+            print(InteractiveCMD._get_output(self.out_queue))
+            print(InteractiveCMD._get_output(self.err_queue))
+
+    def input(self, input_str, verbose=False):
+        if verbose: print('Your input: %s' % input_str)
+        self.p.stdin.write(input_str.strip('\n')+'\n')
+        self.p.stdin.flush()
+        if verbose:
+            self.print_output_error()
+
+    def inputs(self, inputs, intervel=0, verbose=False):
+    	for input_str in inputs:
+    		self.input(input_str, verbose=verbose)
+    		time.sleep(intervel)
+
+    def print_output_error(self):
+        print(InteractiveCMD._get_output(self.out_queue))
+        print(InteractiveCMD._get_output(self.err_queue))
+
+    def _enqueue_output(out, tgt_queue):
+    	for line in iter(out.readline, b''):
+    		tgt_queue.put(line)
+    	out.close()
+
+
+    def _get_output(out_queue):
+    	out_str = ''
+    	try:
+    		while True:
+    			out_str += out_queue.get_nowait()
+    	except queue.Empty:
+    		return out_str
